7da2e0dd8af8d00e423069581c41f06ae817e1fe
doru87 <hurricane.1987@yahoo.com>
2021-04-09 15:53:06 +0300

    initial release

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -39,7 +39,7 @@
                                     </div>
                                 @else
                                     <div class="float-left">
-                                        <img src="/storage{{$_SERVER["DOCUMENT_ROOT"]}}/profile_images/{{$profile->email}}/{{$profile->picture}}" class="img-circle avatar comment-image border border-primary mr-3" alt="user profile image">
+                                        <img src="storage{{$_SERVER["DOCUMENT_ROOT"]}}/profile_images/{{$profile->email}}/{{$profile->picture}}" class="img-circle avatar comment-image border border-primary mr-3" alt="user profile image">
                                     </div>
                                 @endif
                             @endif
